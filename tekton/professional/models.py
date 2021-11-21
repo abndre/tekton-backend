e223cf298065fe59441caf536d5df0dbac6621ef
andre <andrezio@live.com>
2021-11-21 18:33:53 -0300

    update model teste cadastro

--- tekton/professional/models.py
+++ tekton/professional/models.py
@@ -11,6 +11,15 @@ class Professional(models.Model):
     )
 
     ability = models.CharField(max_length=100, choices=ability_choice, blank=False, null=False)
+    telephone = models.CharField(max_length=200, default='', null=True)
+    time_choice = (
+        ("manha", "Manha"),
+        ("tarde", "Tarde")
+    )
+
+    period = models.CharField(max_length=10, choices=time_choice, blank=False, null=False)
+    city = models.CharField(max_length=200, default='', null=True)
+    email = models.CharField(max_length=200, default='', null=True)
 
     def __str__(self):
         return self.name
